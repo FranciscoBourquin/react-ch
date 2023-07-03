2d979000c248648043263847bf01f95f36441eb6
francisco bourquin <fran.bourquin89@gmail.com>
2023-07-03 01:29:26 -0300

    Se organiza la estructura de la página importando los componentes en App.js. Se agregan estilos a App y a index

--- proyecto-react/src/App.js
+++ proyecto-react/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { Carrito } from './Componentes/Carrito/Carrito';
+import { ContenedorProductos } from './Componentes/ContenedorProductos/ContenedorProductos';
+import { Navbar } from './Componentes/Nabvar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="contenedor">
+        <Navbar>
+            <Carrito />
+        </Navbar>
+        <ContenedorProductos
+          greeting = '¡Bienvenidos!'
 
+          />
     </div>
   );
 }
