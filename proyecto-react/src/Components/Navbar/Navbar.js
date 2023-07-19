943b2c50a9f759728d387b1ee18ec02360b76baf
Francisco Bourquin <fran.bourquin89@gmail.com>
2023-07-19 13:11:53 -0300

    Se crea el mock Products que contiene 3 arrays con productos. Se modifican las secciones del Navbar

--- proyecto-react/src/Components/Navbar/Navbar.js
+++ proyecto-react/src/Components/Navbar/Navbar.js
@@ -2,10 +2,11 @@ export const Navbar = ( {children}) => {
   return (
 
     <ul className="navbar">
-  <li><a href="/">Inicio</a></li>
-  <li><a href="/">Productos</a></li>
-  <li><a href="/">{children}</a></li>
-</ul>
+      <li><a href="/">Tecnología</a></li>
+      <li><a href="/">Ropa Masculina</a></li>
+      <li><a href="/">Ropa Femenina</a></li>
+      <li><a href="/">{children}</a></li>
+    </ul>
 
   )
 }
---
+++ proyecto-react/src/Components/Products/Products.js
@@ -0,0 +1,96 @@
+export const womensClothes = [
+    {
+      'id':0 ,
+      'description': 'Campera Gery De Nylon Mujer 47 Street' ,
+      'image': 'https://http2.mlstatic.com/D_NQ_NP_704050-MLA54549278415_032023-O.webp',
+      'price': '$50.399'
+    },
+
+    {
+      'id': 1,
+      'description': 'Buzo De Mujer Mindtrip Oneill' ,
+      'image': 'https://http2.mlstatic.com/D_NQ_NP_914014-MLA69537184354_052023-O.webp',
+      'price': '$18.200'
+    },
+
+    {
+      'id': 2 ,
+      'description': 'Remera Mujer Entrenamiento Deportiva Gym Running Kadur',
+      'image': 'https://http2.mlstatic.com/D_NQ_NP_938532-MLA49077015453_022022-O.webp',
+      'price': '$2.740'
+    },
+
+    {
+      'id': 3 ,
+      'description': 'Pijama Remeron Y Pantalon Otoño Invierno Sol Y Oro 70106sy' ,
+      'image': 'https://http2.mlstatic.com/D_NQ_NP_756016-MLA54915028961_042023-O.webp',
+      'price': '$10.900'
+    }
+]
+
+
+export const mensClothes = [
+{
+    'id': 0,
+    'description': 'Campera Topper Puffer Men Negro',
+    'image': 'https://http2.mlstatic.com/D_NQ_NP_785391-MLA69692746305_052023-O.webp',
+    'price': '$40.899'
+},
+
+{
+'id': 1,
+'description': 'Buzo Canguro Back To Basics Oneill',
+'image': 'https://http2.mlstatic.com/D_NQ_NP_610164-MLA70456016669_072023-O.webp',
+'price': '$13.930'
+},
+
+{
+'id': 2,
+'description': 'Pantalones Hombre Cargo Bolsillos Casuales Jogger Alpina',
+'image': 'https://http2.mlstatic.com/D_NQ_NP_729627-MLA53269627205_012023-O.webp',
+'price': '$6.330'
+},
+
+{
+'id': 3,
+'description': 'Short Fútbol Topper Line Hombre En Gris | Dexter',
+'image': 'https://http2.mlstatic.com/D_NQ_NP_650656-MLA69202768781_052023-O.webp',
+'price': '$6.119'
+}
+]
+
+export const tech = [
+{
+  'id': 0,
+  'description': 'Notebook Lenovo Ideapad 1i Intel I3 1215u 4gb Ram (ampliable Hasta 12gb) 128gb Ssd Windows 11s',
+  'image': 'https://http2.mlstatic.com/D_NQ_NP_787180-MLA54518190876_032023-O.webp',
+  'price': '$198.799'
+},
+
+{
+'id': 1,
+'description': 'Notebook gamer Asus Rog Strix G15 G513RM eclipse gray 15.6", AMD Ryzen 7 6800H 16GB de RAM 512GB SSD, Nvidia RTX 3060 165 Hz 2560x1440px Windows 11 Home',
+'image': 'https://http2.mlstatic.com/D_NQ_NP_806340-MLA51423594714_092022-O.webp',
+'price': '$1.329.999'
+},
+
+{
+'id': 2,
+'description': 'Samsung Galaxy A04e 32 GB negro 3 GB RAM',
+'image': 'https://http2.mlstatic.com/D_NQ_NP_867179-MLA53352656840_012023-O.webp',
+'price': '$56.999'
+},
+
+{
+'id': 3,
+'description': 'Moto G13 Dual SIM 128 GB azul difuso 4 GB RAM',
+'image': 'https://http2.mlstatic.com/D_NQ_NP_986896-MLU69822393367_062023-O.webp',
+'price': '$79.999'
+}
+
+]
+
+
+
+
+
