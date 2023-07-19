37a337767968221bc39513d7bc8e3af96e7d63fb
Francisco Bourquin <fran.bourquin89@gmail.com>
2023-07-19 16:52:46 -0300

    Se crea el logo de la tienda

---
+++ proyecto-react/src/Components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../Images/logo.jpg';
+
+export const Logo = () => {
+  return (
+    <span className='logo'>
+        <img src={logo} alt="Logo" />
+      </span>
+  );
+};
+
