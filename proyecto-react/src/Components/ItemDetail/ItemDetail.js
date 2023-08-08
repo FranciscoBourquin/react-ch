f0cf033ca64aec3ef5daea734aa6dc3e7b638bdf
Francisco Bourquin <fran.bourquin89@gmail.com>
2023-08-08 02:16:06 -0300

    Se crea un estado y se crean dos botones. Los botones reciben la fucnión del estado en un evento y actualizan la cantidad de productos sumando o restando

--- proyecto-react/src/Components/ItemDetail/ItemDetail.js
+++ proyecto-react/src/Components/ItemDetail/ItemDetail.js
@@ -1,17 +1,34 @@
 import { Item } from '../Item/Item';
+import { Button } from '../Button/Button';
+import { useState } from 'react';
 
 export const ItemDetail = ({ name, image, description, price }) => {
+  const [quantity, setQuantity] = useState(0);
+
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleDecrement = () => {
+    setQuantity(quantity - 1);
+  };
+
   return (
     <div>
       <h2>Detalles del producto</h2>
-      <Item
-      name={name}
-      image={image}
-      description={description}
-      price={price} />
+      <Item name={name} image={image} description={description} price={price} />
+      <Button
+      texto= '+'
+       onClick= {handleIncrement}/>
+      <span> {quantity}</span>
+      <Button
+      texto= '-'
+       onClick= {handleDecrement}/>
     </div>
   );
 };
+
+
 
 
 
