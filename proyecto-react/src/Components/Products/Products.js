4ee2fc6ed2f7e66c6a21c9a74575449a053c1d72
Francisco Bourquin <fran.bourquin89@gmail.com>
2023-08-12 21:03:30 -0300

    Se cambia el orden de los arreglos y los ids de los productos

--- proyecto-react/src/Components/Products/Products.js
+++ proyecto-react/src/Components/Products/Products.js
@@ -1,52 +1,77 @@
-export const womensClothes = [
-    {
-      'id':0 ,
-      'name': 'Campera Gery De Nylon Mujer 47 Street' ,
-      'description': 'Campera oversize en nylon, con guata interna y matelaseado. Forrado al tono. Cierre frontal al tono. Bolsillos con snaps. Capucha incorporada, con avios marcarios. Elastico circular en bajo con tanca para ajustar. Multiples bolsillos. Modelo super canchero.',
-      'image': 'https://http2.mlstatic.com/D_NQ_NP_704050-MLA54549278415_032023-O.webp',
-      'price': '$50.399'
-    },
+export const tech = [
+  {
+    'id': 0,
+    'name': 'Notebook Lenovo Ideapad 1i Intel I3 1215u 4gb Ram (ampliable Hasta 12gb) 128gb Ssd Windows 11s',
+    'description': (<div>
+                      Procesador Intel Core i3. <br/>
+                      Memoria RAM de 4GB. <br/>
+                      Pantalla TN de 14". <br/>
+                      Resolución de 1366 x 768p. <br/>
+                      Conexión wifi y bluetooth. <br/>
+                      Cuenta con puerto USB y puerto HDMI. <br/>
+                      La duración de la batería depende del uso que se le dé al producto. <br/>
+                  </div>),
+    'image': 'https://http2.mlstatic.com/D_NQ_NP_787180-MLA54518190876_032023-O.webp',
+    'price': '$198.799'
+  },
 
-    {
-      'id': 1,
-      'name': "Buzo De Mujer Mindtrip O'neill" ,
-      'image': 'https://http2.mlstatic.com/D_NQ_NP_914014-MLA69537184354_052023-O.webp',
-      'description': 'Buzo canguro estampado con tintas ecológicas con capucha embolsada en Jersey. Estampa 1 color en frente, parche de goma marcario sobre bolsillo. Ojalillos metálicos, cordón redondo con puntera metálica marcaria. Cintura y puños con ribb.',
-      'price': '$18.200'
-    },
+  {
+  'id': 1,
+  'name': 'Notebook gamer Asus Rog Strix G15 G513RM eclipse gray 15.6", AMD Ryzen 7 6800H 16GB de RAM 512GB SSD, Nvidia RTX 3060 165 Hz 2560x1440px Windows 11 Home',
+  'description': (<div>
+                    Procesador AMD Ryzen 7. <br />
+                    Memoria RAM de 16GB. <br />
+                    Resolución de 2560x1440 px. <br />
+                    Placa de video Nvidia RTX 3060. <br />
+                    Conexión wifi y bluetooth. <br />
+                    Cuenta con 4 puertos USB y puerto HDMI. <br />
+                    Con teclado retroiluminado. <br />
+                    La duración de la batería depende del uso que se le dé al producto. <br />
+                </div>),
+  'image': 'https://http2.mlstatic.com/D_NQ_NP_806340-MLA51423594714_092022-O.webp',
+  'price': '$1.329.999'
+  },
 
-    {
-      'id': 2 ,
-      'name': 'Remera Mujer Entrenamiento Deportiva Gym Running Kadur',
-      'description': (<div>
-                        - Corte regular fit: corte al cuerpo. <br />
-                        - Confeccionadas en set de poliéster de primera calidad. <br />
-                        - Ideal para entrenamiento funcional, running, crossfit, ciclismo, vóley, entre otros deportes.
+  {
+  'id': 2,
+  'name': 'Samsung Galaxy A04e 32 GB negro 3 GB RAM',
+  'description': (<div>
+                      Dispositivo liberado para que elijas la compañía telefónica que prefieras. <br />
+                      Pantalla PLS de 6.5". <br />
+                      Tiene 2 cámaras traseras de 13Mpx/2Mpx. <br />
+                      Cámara delantera de 5Mpx. <br />
+                      Procesador MediaTek Helio P35 Octa-Core de 2.3GHz con 3GB de RAM. <br />
+                      Batería de 5000mAh. <br />
+                      Memoria interna de 32GB. <br />
+                      Con reconocimiento facial. <br />
+                </div>),
+  'image': 'https://http2.mlstatic.com/D_NQ_NP_867179-MLA53352656840_012023-O.webp',
+  'price': '$56.999'
+  },
 
-                    </div>),
-      'image': 'https://http2.mlstatic.com/D_NQ_NP_938532-MLA49077015453_022022-O.webp',
-      'price': '$2.740'
-    },
+  {
+  'id': 3,
+  'name': 'Moto G13 Dual SIM 128 GB azul difuso 4 GB RAM',
+  'description': (<div>
+                    Dispositivo liberado para que elijas la compañía telefónica que prefieras. <br />
+                    Pantalla IPS de 6.5". <br />
+                    Tiene 3 cámaras traseras de 50Mpx/2Mpx/2Mpx. <br />
+                    Cámara delantera de 8Mpx. <br />
+                    Procesador MediaTek Helio G85 Octa-Core de 2GHz con 4GB de RAM. <br />
+                    Batería de 5000mAh. <br />
+                    Memoria interna de 128GB. <br />
+                    Resistente a las salpicaduras. <br />
+                    Con reconocimiento facial y sensor de huella dactilar. <br />
+                </div>),
+  'image': 'https://http2.mlstatic.com/D_NQ_NP_986896-MLU69822393367_062023-O.webp',
+  'price': '$79.999'
+  }
 
-    {
-      'id': 3 ,
-      'name': 'Pijama Remeron Y Pantalon Otoño Invierno Sol Y Oro 70106sy' ,
-      'description': (<div>
-                        Pijama Otoño - Invierno <br />
-                        Remerón largo de lanilla con abertura a los costados y volados a la altura de los hombros <br />
-                        Pantalón ajustado con abertura en tobillos <br />
-                        ¡Súper versátil! No es solo un pijama más, también podés usarla para hacer otras actividades <br />
-                        Incluye vincha de regalo
-                    </div>),
-      'image': 'https://http2.mlstatic.com/D_NQ_NP_756016-MLA54915028961_042023-O.webp',
-      'price': '$10.900'
-    }
-]
-
+  ]
 
 export const mensClothes = [
 {
-    'id': 0,
+    'id': 4,
     'name': 'Campera Topper Puffer Men Negro',
     'description': 'Campera puffer corta, a la primera cadera, hombro caido. Matelaseado ancho horizontal en frente y espalda. Con capucha y bolsillos laterales. Elástico en puños y cintura. Cierre diente de perro. Logo estampado.',
     'image': 'https://http2.mlstatic.com/D_NQ_NP_785391-MLA69692746305_052023-O.webp',
@@ -54,7 +79,7 @@ export const mensClothes = [
 },
 
 {
-'id': 1,
+'id': 5,
 'name': "Buzo Canguro Back To Basics O'neill",
 'description': 'Buzo canguro con capucha embolsada en Jersey. Parche de goma marcario sobre bolsillo. Ojalillos y punteras metálicas marcarias con cordón redondo a tono. Cintura y puños con ribb. Estampa en frente y en manga a 1 color.',
 'image': 'https://http2.mlstatic.com/D_NQ_NP_610164-MLA70456016669_072023-O.webp',
@@ -62,7 +87,7 @@ export const mensClothes = [
 },
 
 {
-'id': 2,
+'id': 6,
 'name': 'Pantalones Hombre Cargo Bolsillos Casuales Jogger Alpina',
 'description': (<div>
                   COLORES: <br />
@@ -79,7 +104,7 @@ export const mensClothes = [
 },
 
 {
-'id': 3,
+'id': 7,
 'name': 'Short Fútbol Topper Line Hombre En Gris | Dexter',
 'description': 'Confeccionado pensando en tu comodidad y rendimiento, este short cuenta con bolsillos y ajuste con cordón para brindarte la máxima versatilidad. Su diseño te permitirá moverte con libertad y comodidad, mientras su tejido transpirable te mantiene fresco durante el juego.',
 'image': 'https://http2.mlstatic.com/D_NQ_NP_650656-MLA69202768781_052023-O.webp',
@@ -87,78 +112,47 @@ export const mensClothes = [
 }
 ]
 
-export const tech = [
-{
-  'id': 0,
-  'name': 'Notebook Lenovo Ideapad 1i Intel I3 1215u 4gb Ram (ampliable Hasta 12gb) 128gb Ssd Windows 11s',
-  'description': (<div>
-                    Procesador Intel Core i3. <br/>
-                    Memoria RAM de 4GB. <br/>
-                    Pantalla TN de 14". <br/>
-                    Resolución de 1366 x 768p. <br/>
-                    Conexión wifi y bluetooth. <br/>
-                    Cuenta con puerto USB y puerto HDMI. <br/>
-                    La duración de la batería depende del uso que se le dé al producto. <br/>
-                </div>),
-  'image': 'https://http2.mlstatic.com/D_NQ_NP_787180-MLA54518190876_032023-O.webp',
-  'price': '$198.799'
-},
+export const womensClothes = [
+  {
+    'id':8 ,
+    'name': 'Campera Gery De Nylon Mujer 47 Street' ,
+    'description': 'Campera oversize en nylon, con guata interna y matelaseado. Forrado al tono. Cierre frontal al tono. Bolsillos con snaps. Capucha incorporada, con avios marcarios. Elastico circular en bajo con tanca para ajustar. Multiples bolsillos. Modelo super canchero.',
+    'image': 'https://http2.mlstatic.com/D_NQ_NP_704050-MLA54549278415_032023-O.webp',
+    'price': '$50.399'
+  },
 
-{
-'id': 1,
-'name': 'Notebook gamer Asus Rog Strix G15 G513RM eclipse gray 15.6", AMD Ryzen 7 6800H 16GB de RAM 512GB SSD, Nvidia RTX 3060 165 Hz 2560x1440px Windows 11 Home',
-'description': (<div>
-                  Procesador AMD Ryzen 7. <br />
-                  Memoria RAM de 16GB. <br />
-                  Resolución de 2560x1440 px. <br />
-                  Placa de video Nvidia RTX 3060. <br />
-                  Conexión wifi y bluetooth. <br />
-                  Cuenta con 4 puertos USB y puerto HDMI. <br />
-                  Con teclado retroiluminado. <br />
-                  La duración de la batería depende del uso que se le dé al producto. <br />
-              </div>),
-'image': 'https://http2.mlstatic.com/D_NQ_NP_806340-MLA51423594714_092022-O.webp',
-'price': '$1.329.999'
-},
+  {
+    'id': 9,
+    'name': "Buzo De Mujer Mindtrip O'neill" ,
+    'image': 'https://http2.mlstatic.com/D_NQ_NP_914014-MLA69537184354_052023-O.webp',
+    'description': 'Buzo canguro estampado con tintas ecológicas con capucha embolsada en Jersey. Estampa 1 color en frente, parche de goma marcario sobre bolsillo. Ojalillos metálicos, cordón redondo con puntera metálica marcaria. Cintura y puños con ribb.',
+    'price': '$18.200'
+  },
 
-{
-'id': 2,
-'name': 'Samsung Galaxy A04e 32 GB negro 3 GB RAM',
-'description': (<div>
-                    Dispositivo liberado para que elijas la compañía telefónica que prefieras. <br />
-                    Pantalla PLS de 6.5". <br />
-                    Tiene 2 cámaras traseras de 13Mpx/2Mpx. <br />
-                    Cámara delantera de 5Mpx. <br />
-                    Procesador MediaTek Helio P35 Octa-Core de 2.3GHz con 3GB de RAM. <br />
-                    Batería de 5000mAh. <br />
-                    Memoria interna de 32GB. <br />
-                    Con reconocimiento facial. <br />
-              </div>),
-'image': 'https://http2.mlstatic.com/D_NQ_NP_867179-MLA53352656840_012023-O.webp',
-'price': '$56.999'
-},
+  {
+    'id': 10 ,
+    'name': 'Remera Mujer Entrenamiento Deportiva Gym Running Kadur',
+    'description': (<div>
+                      - Corte regular fit: corte al cuerpo. <br />
+                      - Confeccionadas en set de poliéster de primera calidad. <br />
+                      - Ideal para entrenamiento funcional, running, crossfit, ciclismo, vóley, entre otros deportes.
 
-{
-'id': 3,
-'name': 'Moto G13 Dual SIM 128 GB azul difuso 4 GB RAM',
-'description': (<div>
-                  Dispositivo liberado para que elijas la compañía telefónica que prefieras. <br />
-                  Pantalla IPS de 6.5". <br />
-                  Tiene 3 cámaras traseras de 50Mpx/2Mpx/2Mpx. <br />
-                  Cámara delantera de 8Mpx. <br />
-                  Procesador MediaTek Helio G85 Octa-Core de 2GHz con 4GB de RAM. <br />
-                  Batería de 5000mAh. <br />
-                  Memoria interna de 128GB. <br />
-                  Resistente a las salpicaduras. <br />
-                  Con reconocimiento facial y sensor de huella dactilar. <br />
-              </div>),
-'image': 'https://http2.mlstatic.com/D_NQ_NP_986896-MLU69822393367_062023-O.webp',
-'price': '$79.999'
-}
+                  </div>),
+    'image': 'https://http2.mlstatic.com/D_NQ_NP_938532-MLA49077015453_022022-O.webp',
+    'price': '$2.740'
+  },
 
+  {
+    'id': 11 ,
+    'name': 'Pijama Remeron Y Pantalon Otoño Invierno Sol Y Oro 70106sy' ,
+    'description': (<div>
+                      Pijama Otoño - Invierno <br />
+                      Remerón largo de lanilla con abertura a los costados y volados a la altura de los hombros <br />
+                      Pantalón ajustado con abertura en tobillos <br />
+                      ¡Súper versátil! No es solo un pijama más, también podés usarla para hacer otras actividades <br />
+                      Incluye vincha de regalo
+                  </div>),
+    'image': 'https://http2.mlstatic.com/D_NQ_NP_756016-MLA54915028961_042023-O.webp',
+    'price': '$10.900'
+  }
 ]
-
-
-
-
-
