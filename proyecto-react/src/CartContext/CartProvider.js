fe291f738dd62ac0f61c46f269e3f5542d8cb624
Francisco Bourquin <fran.bourquin89@gmail.com>
2023-08-17 18:13:24 -0300

    Se corrigió error con el children

--- proyecto-react/src/CartContext/CartProvider.js
+++ proyecto-react/src/CartContext/CartProvider.js
@@ -1,19 +1,30 @@
 import { useState } from 'react'
 import { CartContext } from './CartContext'
 
-export const CartProvider = (children) => {
+export const CartProvider = ({children}) => {
     //Variabñe para el carrito
     const [cart, setCart]  = useState([])
+
+    //Agregar al carrito
+    const addToCart = () => {}
+
     //Manejar las unidades que se piden
     const [quantity, setQuantity] = useState(0)
 
+    //Quitar del carrito
+    const removeFromCart = (id) => setCart(cart.filter(item => item.id !== id))
 
+    //Vaciar carrito
+    const clearCart = () => {
+      setCart([])
+    }
 
   return (
     <div>
-            <CartContext>
+            <CartContext.Provider
+            value={{cart, setCart, quantity, setQuantity, addToCart, removeFromCart, clearCart}}>
                 {children}
-            </CartContext>
+            </CartContext.Provider>
     </div>
   )
 }
