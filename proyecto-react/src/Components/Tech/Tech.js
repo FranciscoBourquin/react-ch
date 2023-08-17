1f1d4a842f272347c84652a3ef872c7b44970492
Francisco Bourquin <fran.bourquin89@gmail.com>
2023-08-17 16:59:31 -0300

    Se corrige la importación de Products y la obtención de datos del mismo

--- proyecto-react/src/Components/Tech/Tech.js
+++ proyecto-react/src/Components/Tech/Tech.js
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 import { ItemList } from '../ItemList/ItemList';
 import { Item } from '../Item/Item';
-import { tech } from '../Products/Products';
+import { Products } from '../Products/Products.json';
 import { Button } from '../Button/Button';
 
 export const Tech = () => {
   return (
     <ItemList>
-      {tech.map((item) => (
+      {Products.tech.map((item) => (
         <div key={item.id}>
           <Item name={item.name} image={item.image} />
           <Link to={`/Tech/${item.id}`}>
