0d89ddab2fc052e6a10907b9a2ce22b53ffffe13
Francisco Bourquin <fran.bourquin89@gmail.com>
2023-08-17 23:35:56 -0300

    Se crean las funciones decrementQuantity e incrementQuantity

--- proyecto-react/src/CartContext/CartProvider.js
+++ proyecto-react/src/CartContext/CartProvider.js
@@ -1,30 +1,58 @@
-import { useState } from 'react'
-import { CartContext } from './CartContext'
+import { useState } from "react";
+import { CartContext } from '../CartContext/CartContext'
 
 export const CartProvider = ({children}) => {
-    //Variabñe para el carrito
-    const [cart, setCart]  = useState([])
+  const [cart, setCart] = useState([]);
+  const [quantity, setQuantity] = useState(0);
 
-    //Agregar al carrito
-    const addToCart = () => {}
+  const addToCart = (item) => {
+      setCart([...cart, item]);
+  };
 
-    //Manejar las unidades que se piden
-    const [quantity, setQuantity] = useState(0)
+  const removeFromCart = (id) => {
+      setCart(cart.filter(item => item.id !== id));
+  };
 
-    //Quitar del carrito
-    const removeFromCart = (id) => setCart(cart.filter(item => item.id !== id))
+  const incrementQuantity = (id) => {
+      const updatedCart = cart.map(item => {
+          if (item.id === id) {
+              return { ...item, quantity: item.quantity + 1 };
+          }
+          return item;
+      });
+      setCart(updatedCart);
+  };
 
-    //Vaciar carrito
-    const clearCart = () => {
-      setCart([])
-    }
+  const decrementQuantity = (id) => {
+      const updatedCart = cart.map(item => {
+          if (item.id === id) {
+              return { ...item, quantity: item.quantity - 1 };
+          }
+          return item;
+      }).filter(item => item.quantity > 0);
+      setCart(updatedCart);
+  };
+
+  const clearCart = () => {
+      setCart([]);
+  };
 
   return (
-    <div>
-            <CartContext.Provider
-            value={{cart, setCart, quantity, setQuantity, addToCart, removeFromCart, clearCart}}>
-                {children}
-            </CartContext.Provider>
-    </div>
-  )
+      <div>
+          <CartContext.Provider
+              value={{
+                  cart,
+                  setCart,
+                  quantity,
+                  setQuantity,
+                  addToCart,
+                  removeFromCart,
+                  incrementQuantity,
+                  decrementQuantity,
+                  clearCart
+              }}>
+              {children}
+          </CartContext.Provider>
+      </div>
+  );
 }
